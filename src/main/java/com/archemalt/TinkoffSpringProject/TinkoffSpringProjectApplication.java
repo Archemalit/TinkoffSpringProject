ae5d945c22acc16d4def2dde060996a7d5ca61fa
Archemalit <ArsenDivirov@yandex.ru>
2024-08-02 14:47:41 +0500

    init project

---
+++ src/main/java/com/archemalt/TinkoffSpringProject/TinkoffSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.archemalt.TinkoffSpringProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinkoffSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinkoffSpringProjectApplication.class, args);
+	}
+
+}
